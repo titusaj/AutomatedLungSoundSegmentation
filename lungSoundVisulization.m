08d7b2b616b0dd4e4a33a7c820be051e5da6a835
titusaj <titusaj92@gmail.com>
2019-04-29 10:48:08 -0400

    syncing start changes

--- lungSoundVisulization.m
+++ lungSoundVisulization.m
@@ -43,8 +43,8 @@ end
  F = 2048*2;
                  
  figure
- spectrogram(rawWholeSignal,window,noverlap,Fs/2,Fs,'yaxis')
- [s,w,t] = spectrogram(rawWholeSignal,window,noverlap,F/2,F,'yaxis');
+ spectrogram(filter_out,window,noverlap,Fs/2,Fs,'yaxis')
+ [s,w,t] = spectrogram(filter_out,window,noverlap,Fs/2,Fs,'yaxis');
  ylim([0 .5])
  
  for i=1:length(cycleStart)
@@ -59,43 +59,72 @@ end
 
 
 bandPower = [];
+bandEnergy = []; 
 fh = 1000;
 fl = 80;
 for k = 1:length(s(1,:))
 
     rawBand = s(:,k);
     Ex = norm(rawBand,2)^2;  % the energy
+    
     Px = (1/(fh-fl))* 1/numel(rawBand)*norm(rawBand,2)^2; % power
     bandPower(k)= Px; 
+    bandEnergy(k)= Ex; 
 end
 
 
 
-smoothBandPower = smoothn(bandPower,'robust');
+smoothBandPower = smoothn(bandPower,10);
 
 
-% figure
-% hold on
-% plot(t, bandPower)
-% TF = islocalmin(smoothBandPower);
-% plot(t,smoothBandPower,t(TF),smoothBandPower(TF),'r*')
-% 
-%  
-%  for i=1:length(cycleStart)
-%     vline([cycleStart(i)],['g'])
-% end
-% 
-% for i=1:length(cycleEnd)
-%     vline([cycleEnd(i)],['r'])
-% end
-% 
-% figure
-% hold on
-% plot(t,smoothBandPower,t(TF),smoothBandPower(TF),'r*')
-%  for i=1:length(cycleStart)
-%     vline([cycleStart(i)],['g'])
-% end
-% 
-% for i=1:length(cycleEnd)
-%     vline([cycleEnd(i)],['r'])
-% end
+figure
+title('Band Power Unsmooth')
+hold on
+plot(t, bandPower)
+
+%find the local min
+[TFmin, minTimeIndicies] = islocalmin(smoothBandPower);
+plot(t,smoothBandPower,t(TFmin),smoothBandPower(TFmin),'r*')
+
+%find the local max
+[TFmax, maxTimeIndicies] = islocalmax(smoothBandPower);
+plot(t,smoothBandPower,t(TFmax),smoothBandPower(TFmax),'b*')
+
+ 
+ for i=1:length(cycleStart)
+    vline([cycleStart(i)],['g'])
+end
+
+for i=1:length(cycleEnd)
+    vline([cycleEnd(i)],['r'])
+end
+
+figure
+hold on
+
+plot(t,smoothBandPower,t(TFmin),smoothBandPower(TFmin),'r*')
+plot(t,smoothBandPower,t(TFmax),smoothBandPower(TFmax),'b*')
+
+
+for i=1:length(cycleStart)
+    vline([cycleStart(i)],['g'])
+end
+
+for i=1:length(cycleEnd)
+    vline([cycleEnd(i)],['r'])
+end
+
+%% This find the distance between the first minmum and the next max
+timeBandPowerMins = t(TFmin);
+timeBandPowerMaxs = t(TFmax);
+
+bandPowerMins= smoothBandPower(TFmin);
+bandPowerMaxs = smoothBandPower(TFmax);
+
+bandPowerMaxs = bandPowerMaxs(2:end); %match the length of mins
+deltasMinMax = bandPowerMaxs- bandPowerMins;
+
+[deltasMinMax, deltasMinMaxIndicies] = sort(deltasMinMax, 'descend');
+
+
+scatter(timeBandPowerMins(I(1:8)),bandPowerMins(I(1:8)),'k*')
