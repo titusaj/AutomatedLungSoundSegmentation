2307be947767f3dba81ba6845d864ccc50971e0f
Titus John <titusaj92@gmail.com>
2019-05-09 14:03:57 -0400

    syncing conv1d changes

---
+++ trainSegmentation.py
@@ -0,0 +1,12 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+from sklearn.model_selection import StratifiedKFold
+from keras.callbacks import EarlyStopping, ModelCheckpoint
